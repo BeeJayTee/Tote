5b4ad6b1e8d0b72b9374d42373fb6d25cf1f44ce
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2022-09-02 10:04:11 -0400

    Set up Auth Context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ProductsContextProvider } from './context/ProductContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductsContextProvider>
-      <App />
-    </ProductsContextProvider>
+    <AuthContextProvider>
+      <ProductsContextProvider>
+        <App />
+      </ProductsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
