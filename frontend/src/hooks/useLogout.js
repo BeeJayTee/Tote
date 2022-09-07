dda6b9865a57292f190e277db709fac3b1abdc8c
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2022-09-07 12:03:52 -0400

    cleared products context when user signs out

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from "./useAuthContext"
+import { useProductsContext } from "./useProductsContext"
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch:workoutsDispatch } = useProductsContext()
     const logout = () => {
         localStorage.removeItem('user')
 
         dispatch({type: 'LOGOUT'})
+        workoutsDispatch({type: 'SET_PRODUCTS', payload: null})
     }
     return { logout }
 }
