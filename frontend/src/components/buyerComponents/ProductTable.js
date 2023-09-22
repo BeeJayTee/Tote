110b71a5047f8fd76ff9325866832b9830b2fc8c
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2023-09-22 16:12:35 -0400

    grid style changes on ProductTable component

--- frontend/src/components/buyerComponents/ProductTable.js
+++ frontend/src/components/buyerComponents/ProductTable.js
@@ -4,7 +4,7 @@ const ProductTable = ({ products, productsMessage, hidden }) => {
   return (
     <div className={`${hidden} ProductsDisplay`}>
       {productsMessage && <div>{productsMessage}</div>}
-      <div className="flex flex-col lg:flex-row flex-wrap mt-4 gap-4 lg:w-[80%] m-auto xl:w-full justify-between items-center">
+      <div className="grid mx-8 lg:mx-0 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
         {products.map((product, index) => (
           <ProductCard product={product} index={index} key={index} />
         ))}
